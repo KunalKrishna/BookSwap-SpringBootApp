61f52a748f33d86c97d53ccd2611b824d3b3d0bd
Kunal Krishna <kunalkrishna85@gmail.com>
2024-11-12 10:23:08 -0500

    initial project skeleton

---
+++ src/main/java/edu/unc/cs/BookSwap/BookSwapApplication.java
@@ -0,0 +1,25 @@
+package edu.unc.cs.BookSwap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class BookSwapApplication {
+
+	@GetMapping("/")
+	public String welcome(){
+		return "hello!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookSwapApplication.class, args);
+	}
+
+}
+
+
---
+++ src/test/java/edu/unc/cs/BookSwap/BookSwapApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.unc.cs.BookSwap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookSwapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
