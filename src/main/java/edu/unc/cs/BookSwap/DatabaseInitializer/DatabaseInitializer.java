d6fe17a99d15d21213c2fd65a63d554a29f53e13
Kunal Krishna <kunalkrishna85@gmail.com>
2024-12-06 14:58:18 -0500

    sql trigger stmt changed

--- src/main/java/edu/unc/cs/BookSwap/DatabaseInitializer/DatabaseInitializer.java
+++ src/main/java/edu/unc/cs/BookSwap/DatabaseInitializer/DatabaseInitializer.java
@@ -10,7 +10,9 @@ public class DatabaseInitializer {
 
     private final JdbcTemplate jdbcTemplate;
 
-    public DatabaseInitializer(JdbcTemplate jdbcTemplate) { this.jdbcTemplate = jdbcTemplate;}
+    public DatabaseInitializer(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
     @EventListener(ContextRefreshedEvent.class)
     public void initializeDatabase() {
@@ -19,7 +21,22 @@ public class DatabaseInitializer {
 
     private void createPreventExcessBooksTrigger() {
         String dropTriggerSQL = "DROP TRIGGER IF EXISTS prevent_excess_books;";
-        String createTriggerSQL = "CREATE TRIGGER prevent_excess_books BEFORE INSERT ON book_ownership FOR EACH ROW BEGIN DECLARE book_count INT; SELECT COUNT(*) INTO book_count FROM book_ownership WHERE id = NEW.id; IF book_count >= 10 THEN SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'User cannot own more than 10 books.'; END IF; END;";
+        //String createTriggerSQL = "CREATE TRIGGER prevent_excess_books BEFORE INSERT ON book_ownership FOR EACH ROW BEGIN DECLARE book_count INT; SELECT COUNT(*) INTO book_count FROM book_ownership WHERE id = NEW.id; IF book_count >= 10 THEN SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'User cannot own more than 10 books.'; END IF; END;";
+        String createTriggerSQL = """
+                CREATE TRIGGER prevent_excess_books
+                BEFORE INSERT ON book_ownership
+                FOR EACH ROW
+                BEGIN
+                    DECLARE book_count INT;
+                    SELECT COUNT(*) INTO book_count
+                    FROM book_ownership
+                    WHERE id = NEW.id;
+                    IF book_count >= 10 THEN
+                        SIGNAL SQLSTATE '45000'
+                        SET MESSAGE_TEXT = 'User cannot own more than 10 books.';
+                    END IF;
+                END;
+                """;
 
         try {
             // Drop the trigger if it exists
