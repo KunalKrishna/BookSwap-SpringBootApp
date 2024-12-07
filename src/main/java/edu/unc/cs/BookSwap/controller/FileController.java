e886d8870a7bd541022b0c640cc79867df66c493
Kunal Krishna <kunalkrishna85@gmail.com>
2024-12-06 21:12:20 -0500

    deployment chagnes, databsae created by spring using createDatabaseIfNotExist

---
+++ src/main/java/edu/unc/cs/BookSwap/controller/FileController.java
@@ -0,0 +1,22 @@
+package edu.unc.cs.BookSwap.controller;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class FileController {
+
+    @GetMapping("/pdfs/{fileName:.+}")
+    public ResponseEntity<Resource> serveFile(@PathVariable String fileName) {
+        Resource file = new ClassPathResource("static/pdfs/" + fileName);
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getFilename() + "\"")
+                .body(file);
+    }
+
+}
